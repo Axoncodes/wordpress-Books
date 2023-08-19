4dae47239fcd455e37952c741b5539ce9c3ac687
Axoncodes <devops@axoncodes.com>
2023-08-19 12:02:36 +0330

    Enqueuing style and script

--- plugins/books/shortcode-ui/index.php
+++ plugins/books/shortcode-ui/index.php
@@ -1,11 +1,8 @@
 <?php
 function enqueue_shortcode_assets() {
     if (is_singular() && has_shortcode(get_the_content(), 'book_display')) {
-        // Enqueue your CSS
-        wp_enqueue_style('book-display-style', plugin_dir_url(__FILE__) . 'shortcode-ui/style.css');
-        
-        // Enqueue your JavaScript (if needed)
-        wp_enqueue_script('book-display-script', plugin_dir_url(__FILE__) . 'shortcode-ui/script.js', array('jquery'), '1.0', true);
+        wp_enqueue_style('book-display-style', plugin_dir_url(__FILE__) . 'style.css');
+        wp_enqueue_script('book-display-script', plugin_dir_url(__FILE__) . 'script.js', array('jquery'), '1.0', true);
     }
 }
 add_action('wp_enqueue_scripts', 'enqueue_shortcode_assets');
