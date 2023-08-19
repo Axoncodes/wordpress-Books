38439caa924eacea27f643f6e9b5b2e453a513c9
Axoncodes <devops@axoncodes.com>
2023-08-19 13:05:01 +0330

    move the book nav out of the book ahref tag

--- plugins/books/shortcode-ui/template-functions.php
+++ plugins/books/shortcode-ui/template-functions.php
@@ -4,30 +4,32 @@ function de_book_block(
     $title,
     $thumbnail_url,
     $category,
+    $category_link,
     $publish_year,
     $excerpt_context,
     $author_name,
+    $author_link,
     $link,
 ) {
     $thumbnail_block = '';
     if (isset($thumbnail_url)) {
-        $thumbnail_block = "<div class=\"thumbnail-cover\">
-            <img src=\"$thumbnail_url\" />
+        $thumbnail_block = "<div class='thumbnail-cover'>
+            <img src='$thumbnail_url' />
         </div>";
     }
     $publish_year = tag_book_block($publish_year);
-    $cat = tag_book_block($category);
-    $author = tag_book_block($author_name);
+    $cat = tag_book_block($category, $category_link);
+    $author = tag_book_block($author_name, $author_link);
     $title = "<h2>$title</h2>";
     $excerpt = "<p>$excerpt_context</p>";
     
-    $block = "<article class=\"de-book-block\">
-        <a href=\"$link\">
-            <nav>
-                $author
-                $publish_year
-                $cat
-            </nav>
+    $block = "<article class='de-book-block'>
+        <nav>
+            $author
+            $publish_year
+            $cat
+        </nav>
+        <a href='$link'>
             $thumbnail_block
             $title
         </a>
@@ -37,8 +39,9 @@ function de_book_block(
     return $block;
 }
 
-function tag_book_block($text = "") {
-    return "<div class=\"tag\">
-        <span>$text</span>
-    </div>";
+function tag_book_block($text='', $link=null) {
+    $thelink = '';
+    if (isset($link)) $thelink = "href='$link'";
+    $content = "<a $thelink class='tag'>$text</a>";
+    return $content;
 }
